76026bbff7a75d28eb526158ec8395998ab8678b
Emily Dittmer <emilyedittmer@gmail.com>
2019-01-27 11:14:50 -0700

    Initial commit

---
+++ navigation.js
@@ -0,0 +1,10 @@
+var submitButton =
+document.querySelector('#button1');
+
+button1Button.addEventListener('click', function(){
+  var name =
+  document.querySelector('#name').value;
+  var heading =
+  document.querySelector('#dogname1');
+  heading.innerText =
+})
